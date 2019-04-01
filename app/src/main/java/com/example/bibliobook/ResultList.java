dee54fa2d1c6860de219c0c5367ee1513e1a74cf
BILLES GARABEDIAN Guillaume <guillaume.billes.garabedian@efrei.net>
2019-04-01 22:51:27 +0200

    added API call

--- app/src/main/java/com/example/bibliobook/ResultList.java
+++ app/src/main/java/com/example/bibliobook/ResultList.java
@@ -1,6 +1,5 @@
 package com.example.bibliobook;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +7,28 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ResultList extends AppCompatActivity {
 
-    @SuppressLint("WrongThread")
+    private RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_list);
-        TextView text = (TextView)findViewById(R.id.TopBarSearchText);
-        text.setText(getIntent().getStringExtra("SEARCHED"));
+        queue = Volley.newRequestQueue(this);
+        final TextView text = (TextView) findViewById(R.id.TopBarSearchText);
+        String searchedTerms = getIntent().getStringExtra("SEARCHED");
+        text.setText(searchedTerms);
         ImageView imageview1 = findViewById(R.id.returnToSearchArrow);
         imageview1.setOnClickListener(new View.OnClickListener() {
 
@@ -30,7 +42,42 @@ public class ResultList extends AppCompatActivity {
             }
 
         });
+        final TextView textCenter = (TextView) findViewById(R.id.textView3);
+        String url = "https://www.googleapis.com/books/v1/volumes?q=" + searchedTerms;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        textCenter.setText("");
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("items");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject livre = jsonArray.getJSONObject(i);
+                                JSONObject info = livre.getJSONObject("volumeInfo");
+                                String titre = info.getString("title");
+                                JSONArray auteurArray = info.getJSONArray("authors");
+                                String auteur = (String)auteurArray.get(0);
+                                textCenter.append(titre + " de " + auteur + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            textCenter.setText("JSON Parsing Failed");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        textCenter.setText("Erreur lors de l'appel API");
+                        error.printStackTrace();
+                    }
+                });
+        queue.add(jsonObjectRequest);
 
     }
+
+
 }
